46141f60e748f22cb338bf02b2f45227f2ca9a6a
Mateusz Łuczkowski <mateuszluczkowski@gmail.com>
2021-08-06 14:49:26 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+import "../sass/style.scss";
+class Doggo {
+  constructor() {
+    this.apiUrl = "https://dog.ceo/api";
+    this.imgEl = document.querySelector(".featured-dog img");
+    this.backgroundEl = document.querySelector(".featured-dog__background");
+    this.tilesEl = document.querySelector(".tiles");
+    this.spinnerEl = document.querySelector(".spinner");
+    this.init();
+  }
+  showLoading() {
+    this.spinnerEl.classList.add(".spinner--visible");
+  }
+  hideLoading() {
+    this.spinnerEl.classList.remove(".spinner--visible");
+  }
+  listBreeds = () => {
+    return fetch(`${this.apiUrl}/breeds/list/all`)
+      .then((response) => response.json())
+      .then((data) => data.message);
+  };
+
+  getRandomImage = () => {
+    return fetch(`${this.apiUrl}/breeds/image/random`)
+      .then((response) => response.json())
+      .then((data) => data.message);
+  };
+
+  getRandomImageByBreed = (breed) => {
+    return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+      .then((response) => response.json())
+      .then((data) => data.message);
+  };
+  init = () => {
+    this.showLoading();
+    this.getRandomImage().then((src) => {
+      this.imgEl.src = src;
+      this.backgroundEl.style.background = `url(${src})`;
+      // this.hideLoading();
+    });
+    this.showAllBreeds();
+  };
+  addBreed = (breed, subBreed) => {
+    let name;
+    let type;
+    if (typeof subBreed === "undefined") {
+      name = breed;
+      type = breed;
+    } else {
+      name = `${breed} ${subBreed}`;
+      type = `${breed}/${subBreed}`;
+    }
+    const tile = document.createElement("div");
+    tile.classList.add("tiles__tile");
+    const tileContent = document.createElement("div");
+    tileContent.classList.add("tiles__tile-content");
+    tileContent.innerText = name;
+    tileContent.addEventListener("click", () => {
+      this.showLoading();
+      this.getRandomImageByBreed(type).then((src) => {
+        this.imgEl.src = src;
+        this.backgroundEl.style.background = `url(${src})`;
+        // this.hideLoading();
+      });
+    });
+    tile.appendChild(tileContent);
+    this.tilesEl.appendChild(tile);
+  };
+  showAllBreeds = () => {
+    this.listBreeds().then((breeds) => {
+      for (const breed in breeds) {
+        if (breeds[breed].length === 0) {
+          this.addBreed(breed);
+        } else {
+          for (const subBreed of breeds[breed]) {
+            this.addBreed(breed, subBreed);
+          }
+        }
+      }
+    });
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Doggo();
+});
